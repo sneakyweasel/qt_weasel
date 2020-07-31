808c6fa56077ed988e715db10d37ebd2f3bf172f
Philippe Cochin <philippe.cochin@gmail.com>
2020-07-31 20:13:11 +0200

    Added frames

---
+++ components/Frame.vue
@@ -0,0 +1,37 @@
+<template>
+  <b-container class="bv-example-row">
+    <b-row>
+      <b-col>
+        <h3>
+          Frame {{ index }}
+          <small>({{ frame.probability * 100 }}%)</small>
+        </h3>
+        {{ frame.vector }}
+        <div v-if="frame.particles.length > 0">
+          <h4>Photons</h4>
+          <b-table striped hover :items="frame.particles"></b-table>
+        </div>
+
+        <div v-if="frame.absorptions.length > 0">
+          <h4>Absorptions</h4>
+          <b-table striped hover :items="frame.absorptions"></b-table>
+        </div>
+        <hr />
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script lang="ts">
+import * as qt from 'quantum-tensors'
+import { Component, Prop, Vue } from 'vue-property-decorator'
+@Component({
+  components: {},
+})
+export default class FrameComponent extends Vue {
+  @Prop() readonly frame!: qt.Frame
+  @Prop() readonly index!: number
+}
+</script>
+
+<style scoped lang="scss"></style>
--- pages/index.vue
+++ pages/index.vue
@@ -22,8 +22,11 @@
     <hr />
     <b-row>
       <b-col>
-        <h1>Operators</h1>
-        <p>Convert cells into operators</p>
+        <h1>
+          Operators
+          <small>({{ sim.operators.length }})</small>
+        </h1>
+        <p>Convert cells into operators, display non one values.</p>
         <b-table striped hover :items="sim.operators"></b-table>
       </b-col>
     </b-row>
@@ -31,9 +34,38 @@
     <hr />
     <b-row>
       <b-col>
-        <h1>Global Operators</h1>
+        <h1>
+          Global Operators
+          <small>({{ sim.globalOperator.entries.length }})</small>
+        </h1>
         <p>Merge operators into a global operator</p>
-        {{ sim.globalOperator }}
+        {{ sim.globalOperator.entries.length }} entries
+        <br />
+        {{ sim.globalOperator.entries[0] }}
+      </b-col>
+    </b-row>
+
+    <hr />
+    <b-row>
+      <b-col>
+        <h1>Fire laz0rs!</h1>
+        <ul>
+          <li>Convert laser cell to an IIndicator.</li>
+        </ul>
+        <b-table striped hover :items="[laserIndicator]"></b-table>
+      </b-col>
+    </b-row>
+
+    <hr />
+    <b-row>
+      <b-col>
+        <h1>
+          Frames
+          <small>({{ sim.frames.length }})</small>
+        </h1>
+        <div v-for="(frame, index) in sim.frames" :key="'frame' + index">
+          <FrameComponent :frame="frame" :index="index" />
+        </div>
       </b-col>
     </b-row>
   </b-container>
@@ -42,18 +74,19 @@
 <script lang="ts">
 import * as qt from 'quantum-tensors'
 import { Component, Vue } from 'vue-property-decorator'
+import FrameComponent from '@/components/Frame.vue'
 @Component({
-  components: {},
+  components: { FrameComponent },
 })
 export default class SimulationPage extends Vue {
   grid1 = {
-    cols: 13,
-    rows: 10,
+    cols: 15,
+    rows: 15,
     cells: [
       {
         coord: {
-          y: 2,
-          x: 3,
+          y: 1,
+          x: 1,
         },
         element: 'Laser',
         rotation: 0,
@@ -61,8 +94,17 @@ export default class SimulationPage extends Vue {
       },
       {
         coord: {
-          y: 2,
-          x: 9,
+          y: 1,
+          x: 3,
+        },
+        element: 'Mirror',
+        rotation: 135,
+        polarization: 0,
+      },
+      {
+        coord: {
+          y: 3,
+          x: 3,
         },
         element: 'Detector',
         rotation: 180,
@@ -72,8 +114,9 @@ export default class SimulationPage extends Vue {
   }
 
   sim = new qt.Simulation(this.grid1)
-
-  operators = this.sim.operators
+  laserIndicator = this.sim.initializeFromLaser()
+  initFrame = this.sim.lastFrame
+  test = this.sim.generateFrames()
 }
 </script>
 
